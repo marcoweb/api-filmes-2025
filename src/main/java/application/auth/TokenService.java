8d66546dc704b892ed71cc4aea6749bcddad6235
ETEC <164@etec.sp.gov.br>
2025-10-20 17:47:54 +0000

    Aula 20/10/2025

--- src/main/java/application/auth/TokenService.java
+++ src/main/java/application/auth/TokenService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class TokenService {
@@ -20,7 +21,7 @@ public class TokenService {
 
     public String generateToken(Usuario usuario) {
         try {
-            Algorithm algorithm = Algorithm.HMAC256(tokenKey);
+            Algorithm algorithm = Algorithm.HMAC256(this.tokenKey);
 
             return JWT.create()
                 .withIssuer("Filmes API")
@@ -29,6 +30,19 @@ public class TokenService {
                 .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar o token");
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.tokenKey);
+            return JWT.require(algorithm)
+                .withIssuer("Filmes API")
+                .build()
+                .verify(token)
+                .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token Inválido");
         }
     }
     
